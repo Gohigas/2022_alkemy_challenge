d57cbd02cc2accc74d7f09acbbf0ab477ca8050c
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-06-27 20:34:20 -0300

    Use Environment Variables in -database create-

--- database_create.py
+++ database_create.py
@@ -4,6 +4,7 @@ import sys
 import shelve
 import pandas as pd
 from sqlalchemy import create_engine
+import psycopg2
 
 os.chdir(sys.path[0])
 
@@ -11,9 +12,10 @@ shelf_file = shelve.open('temp/df_processed')
 df_processed_Dict = shelf_file['df_processed_Dict']
 shelf_file.close()
 
-password = None
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASS')
 
-engine = create_engine('postgresql://postgres:{password}@localhost:5432/alkemy_da')
+engine = create_engine(f'postgresql://{db_user}:{db_password}@localhost:5432/alkemy_da')
 
 for name, df in df_processed_Dict.items():
     df.to_sql(name, engine, if_exists = 'replace', index = False)
