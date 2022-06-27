775dac7651275ac1f53c3314d1d18ef8522a6ba1
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-06-27 17:20:16 -0300

    Initial Commit

---
+++ data_process.py
@@ -0,0 +1,171 @@
+import os
+import sys
+import shelve
+import pandas as pd
+from pathlib import Path
+import re
+import logging
+
+
+# -- FUNCTIONS --
+
+def columns_process(category, df, original_columns_Dict):
+    rename_Dict = {}
+    for k, v in original_columns_Dict.items():
+        if k in df.columns:
+            pass
+        else:
+            try:
+                reg_ex = re.compile(v, re.I)
+                rename_Dict[list(filter(reg_ex.match, list(df)))[0]] = k
+            except IndexError as err:
+                print(f'Error: no matching for \'{k}\' in \'{category}\' dataframe')
+                sys.exit(err)
+    
+    df_new = df.rename(columns = rename_Dict)
+    df_new = df_new.drop(df_new.columns.difference(list(original_columns_Dict.keys())), axis = 1)
+
+    return df_new
+
+
+# -- INIT CONFIG --
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -  %(levelname)s -  %(message)s')
+logging.disable(logging.CRITICAL)
+
+os.chdir(sys.path[0])
+logging.debug(f'CWD is {Path.cwd()}')
+
+
+# -- SCRIPT --
+
+shelf_file = shelve.open('data/temp/last_files')
+files_path_Dict = shelf_file['files_path_Dict']
+shelf_file.close()
+
+df_Dict = {}
+df_processed_Dict = {}
+for k, v in files_path_Dict.items():
+    df_Dict[k] = pd.read_csv(v, encoding='utf-8', quotechar='"', na_values=['s/d', 'nan'])
+
+# - Task 01 -
+original_columns_Dict1 ={
+    'cod_localidad': r'cod_loc',
+    'id_provincia': r'idprovincia',
+    'id_departamento': r'iddepartamento',
+    'categoría': r'categoria|categoría',
+    'provincia': r'provincia',
+    'localidad': r'localidad',
+    'nombre': r'nombre',
+    'domicilio': r'domicilio|direccion|dirección',
+    'código postal': r'cp',
+    'número de teléfono': r'telefono|teléfono',
+    'mail': r'mail',
+    'web': r'web'
+}
+
+df_total_Dict = {}
+for category, df in df_Dict.items():
+    df_new = columns_process(category, df, original_columns_Dict1)
+    df_total_Dict[category] = df_new 
+
+for df in df_total_Dict.values():
+    assert list(next(iter(df_total_Dict.values()))) == list(df)
+
+dframes = list(df_total_Dict.values())
+df_total = pd.concat(dframes)
+
+logging.debug(df_total.sample(n=3))
+logging.debug(df_total.shape)
+
+df_processed_Dict["df_total"] = df_total
+
+### Task 02:
+original_columns_Dict2 ={
+    'categoría': r'categoria|categoría',
+    'provincia': r'provincia',
+    'nombre': r'nombre',
+    'fuente': r'fuente'
+}
+
+df_total_Dict = {}
+for category, df in df_Dict.items():
+    df_new = columns_process(category, df, original_columns_Dict2)
+    df_total_Dict[category] = df_new 
+
+for df in df_total_Dict.values():
+    assert list(next(iter(df_total_Dict.values()))) == list(df)
+
+dframes = list(df_total_Dict.values())
+df_temp = pd.concat(dframes)
+
+logging.debug(df_temp.sample(n=3))
+logging.debug(df_temp.shape)
+
+df_categoria = df_temp.groupby('categoría', as_index=False).size()
+df_categoria.rename(columns = {"categoría" : "referencia", "size" : "registros"}, inplace = True)
+
+df_fuente = df_temp.groupby('fuente', as_index=False).size()
+df_fuente.rename(columns = {"fuente" : "referencia", "size" : "registros"}, inplace = True)
+
+df_prov_categoria = df_temp.groupby(['provincia', 'categoría'], as_index=False).size()
+df_prov_categoria.insert(loc = 0, column = "referencia", value = df_prov_categoria[['provincia', 'categoría']].agg('_'.join, axis=1))
+df_prov_categoria.rename(columns = {"size" : "registros"}, inplace = True)
+df_prov_categoria.drop(["provincia", "categoría"], axis = 1, inplace = True)
+
+df_registros = pd.concat([df_categoria, df_fuente, df_prov_categoria])
+
+df_processed_Dict["df_registros"] = df_registros
+
+### Task 03:
+original_columns_Dict3 ={
+    'provincia': r'provincia',
+    'cantidad de pantallas': r'pantallas',
+    'cantidad de butacas': r'butacas',
+    'cantidad de espacios INCAA': r'espacio_INCAA' 
+}
+
+df_lugares_cines = columns_process("cines", df_Dict["cines"], original_columns_Dict3)
+
+
+logging.debug(df_lugares_cines["cantidad de espacios INCAA"].unique())
+df_lugares_cines["cantidad de espacios INCAA"] = df_lugares_cines["cantidad de espacios INCAA"].str.lower()
+df_lugares_cines["cantidad de espacios INCAA"] = df_lugares_cines["cantidad de espacios INCAA"].map({'0': 0, 'si': 1})
+logging.debug(df_lugares_cines["cantidad de espacios INCAA"].unique())
+
+df_lugares_cines["cantidad de espacios INCAA"] = pd.to_numeric(df_lugares_cines["cantidad de espacios INCAA"]).astype('Int64')
+
+df_lugares_cines = df_lugares_cines.groupby(["provincia"], as_index=False)[
+    ["cantidad de pantallas", "cantidad de butacas", "cantidad de espacios INCAA"]
+    ].sum()
+
+df_processed_Dict["df_lugares_cines"] = df_lugares_cines
+
+### Save df_processed_Dict
+shelf_file = shelve.open('data/temp/df_processed')
+shelf_file['df_processed_Dict'] = df_processed_Dict
+shelf_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ data_request.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import requests
+import datetime
+import shelve
+from pathlib import Path
+
+
+# -- FUNCTIONS --
+
+def url_request(category, url):
+    try:
+        res = requests.get(url, allow_redirects=True)
+        res.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
+
+    file_save(category, res)
+    return
+
+def file_save(category, res):
+    month_Names = {
+        1: "enero",
+        2: "febrero",
+        3: "marzo",
+        4: "abril",
+        5: "mayo",
+        6: "junio",
+        7: "julio",
+        8: "agosto",
+        9: "septiembre",
+        10: "octubre",
+        11: "noviembre",
+        12: "diciembre",
+    }
+
+    today = datetime.datetime.now()
+
+    folder_name = "-".join([str(today.year), month_Names[today.month]])
+    file_name = "-".join(
+        [category, str(today.day).zfill(2), str(today.month).zfill(2), str(today.year)]
+    )
+    file_path = Path.cwd() / "data" / category / folder_name
+
+    # Create folder:
+    if not os.path.isdir(file_path):
+        os.makedirs(file_path)
+
+    # Save file:
+    with open(file_path / "".join([file_name, ".csv"]), "wb") as output_file:
+        for chunk in res.iter_content(100000):
+            output_file.write(chunk)
+    
+    files_path_Dict[category] = file_path / "".join([file_name, ".csv"])
+    return
+
+
+# -- URLs --
+url_Dict = {
+    "museos": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv",
+    "cines": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv",
+    "bibliotecas": "https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv"
+}
+
+
+# -- INIT CONFIG --
+os.chdir(sys.path[0])
+if not os.path.isdir(Path.cwd() / 'data'):
+    os.makedirs(Path.cwd() / 'data' / 'temp')
+
+
+# -- SCRIPT --
+shelf_file = shelve.open('data/temp/last_files')
+files_path_Dict = {}
+
+for k, v in url_Dict.items():
+    url_request(k, v)
+
+shelf_file['files_path_Dict'] = files_path_Dict
+shelf_file.close()
