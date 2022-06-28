c6a0e9962497ae0cefcb5d06782d498687dacef1
Juan Cruz <ferreyra.juancruz95@gmail.com>
2022-06-28 14:38:46 -0300

    Use -decouple- in -database_create-

--- database_create.py
+++ database_create.py
@@ -2,18 +2,17 @@ from operator import index
 import os
 import sys
 import shelve
-import pandas as pd
 from sqlalchemy import create_engine
-import psycopg2
 import time
 import logging
 from pathlib import Path
+from decouple import config
 
 
 # -- INIT CONFIG --
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s -  %(levelname)s -  %(message)s")
-logging.disable(logging.CRITICAL)
+# logging.disable(logging.CRITICAL)
 
 os.chdir(sys.path[0])
 logging.debug(f"CWD is {Path.cwd()}")
@@ -25,30 +24,20 @@ shelf_file = shelve.open("temp/df_processed")
 df_processed_Dict = shelf_file["df_processed_Dict"]
 shelf_file.close()
 
-while True:
-    # ask database user
-    db_user = input("Ingrese el usuario de su base de datos(predeterminado: 'postgres'):")
-    if db_user == "":
-        db_user = "postgres"
+db_user = config('DB_USER')   
+db_password = config('DB_PASS')
 
-    # ask database password    
-    db_password = input("Ingrese la contraseña de su base de datos(predeterminado: 'password'):")
-    if db_password == "":
-        db_password = 'password'
-    
-    time.sleep(0.25)
+logging.debug(f"{db_user} : {db_password}")
         
-    engine = create_engine(f"postgresql://{db_user}:{db_password}@localhost:5432/alkemy_da")
+engine = create_engine(f"postgresql://{db_user}:{db_password}@localhost:5432/alkemy_da")
     
-    try:
-        for name, df in df_processed_Dict.items():
-            df.to_sql(name, engine, index=False, if_exists="replace")
-            print(f'{name} actualizada satisfactoriamente...')
-    except Exception:
-        print("Usuario o Contraseña incorrectos \n")
-        time.sleep(0.5)
-        continue
-
-    break
+try:
+    for name, df in df_processed_Dict.items():
+        df.to_sql(name, engine, index=False, if_exists="replace")
+        print(f'{name} actualizada satisfactoriamente...')
+except Exception as err:
+    print("Usuario o Contraseña incorrectos \n")
+    time.sleep(0.5)
+    raise SystemExit(err)
 
 print("Actualización de bases de datos completada.")
